965924c326298bd7342ab277af61eb6261a6954d
Wilson Wise <wilson.wise@turner.com>
2016-09-30 11:39:05 -0400

    simple change for new version

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Demo App', welcome: 'Harbor' });
 });
 
+router.get('/view/:name', (req, res, next) => {
+  res.render('hello', { title: 'Deme App View', name: req.params.name || 'World' })
+});
+
 router.get('/:name', (req, res, next) => {
   res.render('hello', { title: 'Demo App', name: req.params.name || 'World' })
 });
